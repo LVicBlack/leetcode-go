8002d419f6fa6d2ba953ba7843b02f481aba6ec9
vic <vicliu014@gmail.com>
2023-08-09 17:47:12 +0800

    feat: 14 添加注释

--- 14.最长公共前缀/solution_test.go
+++ 14.最长公共前缀/solution_test.go
@@ -41,14 +41,19 @@ import (
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func longestCommonPrefix(strs []string) string {
+	// 第一个字符串作为基础字符串
 	res := strs[0]
 	for i := 1; i < len(strs); i++ {
+		// 结果是“”提前返回
 		if len(res) == 0 {
 			break
 		}
+		// 此轮比较的字符串
 		cur := strs[i]
 		index := 0
+		// 两个字符串的最小长度
 		short := min(len(cur), len(res))
+		// 在最小长度范围内比较两个字符串的公共最长前缀
 		for (index < short) && cur[index] == res[index] {
 			index++
 		}
